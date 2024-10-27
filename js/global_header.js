5dae09b18d0493acdb99f706eeaad8df0b52f621
ArbeiterKlassen <deepdarkfantasyvan@outlook.com>
2024-10-27 21:38:06 +0800

    bug fixed

--- js/global_header.js
+++ js/global_header.js
@@ -30,7 +30,7 @@ document.write('<div class="back-to-top cd-top faa-float animated cd-is-visible"
 
 if(mode==1){
     document.write('<!--PC导航栏-->')
-    document.write('<nav class="nav-bar-holder" style="background-color:#303643">')
+    document.write('<nav class="nav-bar-holder" style="background-color:#63636320">')
     document.write('    <!--<img src="https://i.postimg.cc/RFyJLX7P/headimg.jpg" alt="Head Image" style="text-align: left;width:auto">-->')
     document.write('    <div class="nav-bar" id="nav-bar">')
     document.write('        <div class="logo-box">')
@@ -72,7 +72,7 @@ if(mode==1){
     document.write('</ul>')
 }
 if(mode==2){
-    document.write('<nav class="nav-bar-holder" style="background-color:#303643">')
+    document.write('<nav class="nav-bar-holder" style="background-color:#63636320">')
     document.write('    <!--<img src="https://i.postimg.cc/RFyJLX7P/headimg.jpg" alt="Head Image" style="text-align: left;width:auto">-->')
     document.write('    <div class="nav-bar" id="nav-bar">')
     document.write('        <div class="logo-box">')
