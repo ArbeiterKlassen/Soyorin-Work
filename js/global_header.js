23c501b761ee4a27d746f8b07a3c3af3f6757701
ArbeiterKlassen <deepdarkfantasyvan@outlook.com>
2024-10-29 19:08:23 +0800

    啊嗯

--- js/global_header.js
+++ js/global_header.js
@@ -30,7 +30,7 @@ document.write('<div class="back-to-top cd-top faa-float animated cd-is-visible"
 
 if(mode==1){
     document.write('<!--PC导航栏-->')
-    document.write('<nav class="nav-bar-holder" style="background-color:#63636320">')
+    document.write('<nav class="nav-bar-holder">')
     document.write('    <!--<img src="https://i.postimg.cc/RFyJLX7P/headimg.jpg" alt="Head Image" style="text-align: left;width:auto">-->')
     document.write('    <div class="nav-bar" id="nav-bar">')
     document.write('        <div class="logo-box">')
@@ -40,7 +40,7 @@ if(mode==1){
     document.write('        <div class="nb-a-holder" id="home"><a class="nb-a ta-c" href="../templates/about.html">关于</a></div>')
     document.write('        <div class="nb-a-holder" id="home"><a class="nb-a ta-c" href="../templates/web.sign.html">公告</a></div>')
     document.write('        <div class="nb-a-holder"><a class="nb-a ta-c" href="../templates/category.html">文章分类</a></div>')
-    document.write('        <div class="nb-a-holder"><a class="nb-a ta-c" href="../templates/archive.html">文件归档</a></div>')
+    document.write('        <div class="nb-a-holder"><a class="nb-a ta-c" href="../templates/archive.html">归档</a></div>')
     document.write('        <div class="nb-a-holder"><a class="nb-a ta-c" href="../templates/issue.html">讨论</a></div>')
     document.write('        <div class="nb-a-holder"><a class="nb-a ta-c" href="../index.html">导航页</a></div>')
     document.write('        <div class="nb-a-holder"><a class="nb-a ta-c" href="../templates/link.html">友情链接</a></div>')
@@ -63,7 +63,7 @@ if(mode==1){
     document.write('    <li><a class="w-h-100 fl" href="../templates/about.html">关于</a></li>')
     document.write('    <li><a class="w-h-100 fl" href="../templates/web.sign.html.html">公告板</a></li>')
     document.write('    <li><a class="w-h-100 fl" href="../templates/category.html">文章分类</a></li>')
-    document.write('    <li><a class="w-h-100 fl" href="../templates/archive.html">文件归档</a></li>')
+    document.write('    <li><a class="w-h-100 fl" href="../templates/archive.html">归档</a></li>')
     document.write('    <li><a class="w-h-100 fl" href="../templates/issue.html">讨论</a></div>')
     document.write('    <li><a class="w-h-100 fl" href="../index.html">导航页</a></div>')
     document.write('    <li><a class="w-h-100 fl" href="../templates/link.html">友情链接</a></li>')
@@ -108,7 +108,7 @@ if(mode==1){
     document.write('</div>')
 }
 if(mode==2){
-    document.write('<nav class="nav-bar-holder" style="background-color:#63636320">')
+    document.write('<nav class="nav-bar-holder" style=">')
     document.write('    <!--<img src="https://i.postimg.cc/RFyJLX7P/headimg.jpg" alt="Head Image" style="text-align: left;width:auto">-->')
     document.write('    <div class="nav-bar" id="nav-bar">')
     document.write('        <div class="logo-box">')
@@ -118,7 +118,7 @@ if(mode==2){
     document.write('        <div class="nb-a-holder" id="home"><a class="nb-a ta-c" href="../templates/about.html">关于</a></div>')
     document.write('        <div class="nb-a-holder" id="home"><a class="nb-a ta-c" href="../templates/web.sign.html">公告</a></div>')
     document.write('        <div class="nb-a-holder"><a class="nb-a ta-c" href="../templates/category.html">文章分类</a></div>')
-    document.write('        <div class="nb-a-holder"><a class="nb-a ta-c" href="../templates/archive.html">文件归档</a></div>')
+    document.write('        <div class="nb-a-holder"><a class="nb-a ta-c" href="../templates/archive.html">归档</a></div>')
     document.write('        <div class="nb-a-holder"><a class="nb-a ta-c" href="../templates/issue.html">讨论</a></div>')
     document.write('        <div class="nb-a-holder"><a class="nb-a ta-c" href="../index.html">导航页</a></div>')
     document.write('        <div class="nb-a-holder"><a class="nb-a ta-c" href="../templates/link.html">友情链接</a></div>')
@@ -141,7 +141,7 @@ if(mode==2){
     document.write('    <li><a class="w-h-100 fl" href="../templates/about.html">关于</a></li>')
     document.write('    <li><a class="w-h-100 fl" href="../templates/web.sign.html.html">公告板</a></li>')
     document.write('    <li><a class="w-h-100 fl" href="../templates/category.html">文章分类</a></li>')
-    document.write('    <li><a class="w-h-100 fl" href="../templates/archive.html">文件归档</a></li>')
+    document.write('    <li><a class="w-h-100 fl" href="../templates/archive.html">归档</a></li>')
     document.write('    <li><a class="w-h-100 fl" href="../templates/issue.html">讨论</a></div>')
     document.write('    <li><a class="w-h-100 fl" href="../index.html">导航页</a></div>')
     document.write('    <li><a class="w-h-100 fl" href="../templates/link.html">友情链接</a></li>')
